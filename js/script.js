12a5686071132fb8b57c50737539c300a3f22c43
nathaniel mordetzky <njmordetzky@iMac.local>
2023-04-11 15:01:09 -0500

    tv show details page updated

--- js/script.js
+++ js/script.js
@@ -79,6 +79,8 @@ async function displayMovieDetails() {
     const movieID = window.location.search.split('=')[1];
     console.log(movieID);
     const movie = await fetchAPIData(`movie/${movieID}`);
+    // overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
 
     const div = document.createElement('div');
     div.innerHTML = `
@@ -138,6 +140,84 @@ async function displayMovieDetails() {
     document.querySelector('#movie-details').appendChild(div);
 }
 
+// Display Show Details
+async function displayShowDetails() {
+    const showID = window.location.search.split('=')[1];
+    console.log(showID);
+    const show = await fetchAPIData(`tv/${showID}`);
+    // overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+        <div>
+        ${
+            show.poster_path 
+                ?
+                `<img
+                    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                    class="card-img-top"
+                    alt="${show.name}"
+                />`
+                :
+                `<img
+                    src="images/no-image.jpg"
+                    class="card-img-top"
+                    alt="${show.name}"
+                />`
+        }
+        </div>
+        <div>
+        <h2>${show.name}</h2>
+        <p>
+            <i class="fas fa-star text-primary"></i>
+            ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Air Date: ${show.last_air_date}</p>
+        <p>
+           ${show.overview} 
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+            ${show.genres.map((genre) => 
+                `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+        </div>
+    </div>
+    <div class="details-bottom">
+        <h2>Show Info</h2>
+        <ul>
+        <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+        <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}</li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">
+            ${show.production_companies.map((company) => `
+                <span>${company.name}</span>
+            `).join('')}
+        </div>
+    </div>
+    `;
+
+    document.querySelector('#show-details').appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.classList.add('background-image');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);  
+    }
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = '63efb783f2eefda8ceea8307d905a2ae';
@@ -187,6 +267,7 @@ function init() {
             break;
         case '/javascript/flixx-app/tv-details.html':
             console.log('TV Details');
+            displayShowDetails();
             break;
         case '/javascript/flixx-app/search.html':
             console.log('Search');
